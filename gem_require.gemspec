351c1f2d1423badf29066a1e1c4073af7b7eb547
Mike Williams <mdub@dogbiscuit.org>
2010-12-05 23:24:54 +1100

    Add the '--latest' option.

--- gem_require.gemspec
+++ gem_require.gemspec
@@ -3,7 +3,7 @@
 Gem::Specification.new do |gem|
   
   gem.name = "gem_require"
-  gem.version = "0.0.1"
+  gem.version = "0.0.2"
   gem.platform = Gem::Platform::RUBY
 
   gem.homepage = "http://github.com/mdub/gem_require"
--- lib/rubygems/commands/require_command.rb
+++ lib/rubygems/commands/require_command.rb
@@ -2,6 +2,7 @@ require 'rubygems/command'
 require 'rubygems/dependency_installer'
 require 'rubygems/install_update_options'
 require 'rubygems/version_option'
+require 'rubygems/spec_fetcher'
 
 class Gem::Commands::RequireCommand < Gem::Command
 
@@ -16,13 +17,19 @@ class Gem::Commands::RequireCommand < Gem::Command
       :format_executable => false,
       :test              => false,
       :version           => Gem::Requirement.default,
+      :latest            => false,
     }
     )
 
     super 'require', 'Install or update a gem', defaults
 
-    add_install_update_options
     add_version_option
+    add_option('--latest',
+               'Get the latest matching version') do |value, options|
+      options[:latest] = true
+    end
+
+    add_install_update_options
 
   end
 
@@ -66,23 +73,41 @@ and installs them if necessary.
 
   end
   
+  private
+
   def require_gem(name, version, installer_options)
-
-    dependency = Gem::Dependency.new(name, version)
-
-    if !Gem.source_index.search(dependency).empty?
-      say "#{dependency} is already installed"
-      return false
+    if installed_version = gem_uptodate?(name, version)
+      say "#{name} (#{installed_version}) is already installed"
+    else
+      say "Installing #{name} ..."
+      install_gem(name, version, installer_options)
     end
+  end
 
-    say "Installing #{dependency} ..."
+  def gem_uptodate?(name, version)
+    dependency = Gem::Dependency.new(name, version)
+    installed = current_spec(dependency)
+    if installed 
+      if !options[:latest] || installed.version == latest_available_version_of(dependency)
+        installed.version
+      end
+    end
+  end
 
+  def current_spec(dependency)
+    Gem.source_index.search(dependency).last
+  end
+  
+  def latest_available_version_of(dependency)
+    Gem::SpecFetcher.fetcher.fetch(dependency).map { |x| x.first.version }.max
+  end
+  
+  def install_gem(name, version, installer_options)
     installer = Gem::DependencyInstaller.new(installer_options)
     installer.install(name, version)
-
     installer.installed_gems.each do |spec|
       say "Installed #{spec.full_name}"
     end
   end
-
+  
 end
